a8f1fb4c09cda0f1b24dab5b858949f114b8d355
Rudra Patel <patelrudra219@gmail.com>
2022-05-26 14:23:55 +0530

    added a file that adds two numbers obtained from the user

---
+++ add.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    c = a + b;
+
+    cout << "Sum of " << a << " and " << b << " is " << c << endl;
+    return 0;
+}
